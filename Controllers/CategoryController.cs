a85ff3bcbd8a5ce298d549878eda2ea2f8d7297e
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2021-02-10 11:49:27 +0600

    Initial commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,118 @@
+﻿using BookList_Manager.Data;
+using BookList_Manager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_Manager.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DatabaseConnection _db;
+
+        public CategoryController(DatabaseConnection db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objlist = _db.Category;
+            return View(objlist);
+        }
+
+        // get_create
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        //post-create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+            
+        }
+
+        // get_Edit
+        public IActionResult Edit( int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //post-update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+
+
+        // get_Edit
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //post-delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+                
+
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+           
+
+        }
+    }
+}
+
---
+++ Data/DatabaseConnection.cs
@@ -0,0 +1,19 @@
+﻿using BookList_Manager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_Manager.Data
+{
+    public class DatabaseConnection : DbContext
+    {
+        public DatabaseConnection(DbContextOptions<DatabaseConnection> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
